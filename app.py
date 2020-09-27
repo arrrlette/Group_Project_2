56e80704a8b428ca3f5d01aab20578811321adbd
kickboxerva <kickboxerva@yahoo.com>
2020-09-27 14:24:03 -0400

    Working Files - Anthony

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, redirect, url_for, Response, jsonify
+from bson import json_util
+from flask_pymongo import PyMongo
+from flask_cors import CORS, cross_origin
+import requests
+import json
+
+
+app = Flask(__name__)
+CORS(app, resources={
+    r"/*": {
+        "origins": "*"
+    }
+})
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_ORIGINS'] = '*'
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/super_db"
+mongo = PyMongo(app)
+
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+#This is not recommended in production
+#What would happen is every time you visit the root route it would load the DB again with all the data
+#
+#@app.route("/", methods=["GET"])
+# def index():
+#     # Define database and collection
+#     #super_db = client.super_db
+#     #supestats = super_db.supestats
+
+#     dogcollection = mongo.db.alldogs 
+#     response = requests.get("https://dog.ceo/api/breeds/list/all")
+#     # print(response.json())
+#     responseJson = response.json()
+#     dogcollection.insert(responseJson)
+#     # return render_template("index.html", mars=mars)
+
+
+@app.route("/super_db/", methods=['GET'])
+@cross_origin()
+def allgenders():
+    superdb = mongo.db.super_db
+    # mars_data = scrape_mars.scrape_all()
+    # mars.update({}, mars_data, upsert=True)
+    allgenders = superdb.find({})
+    dogsjson = json.loads(json_util.dumps(alldogs))
+    return jsonify(dogsjson)
+
+if __name__ == "__main__":
+    app.run()
